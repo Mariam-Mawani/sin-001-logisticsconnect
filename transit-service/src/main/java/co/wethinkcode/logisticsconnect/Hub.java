1e2eb319014d1b8639ad2d0363815adbaea08a13
mariam mawani <mariammawani@gmail.com>
2026-09-24 20:20:24 +0200

    Add: fields

---
+++ transit-service/src/main/java/co/wethinkcode/logisticsconnect/Hub.java
@@ -0,0 +1,12 @@
+package co.wethinkcode.logisticsconnect;
+
+public class Hub {
+
+    public String hubId;
+    public String province;
+    public String sortingCenter;
+    public Boolean active;
+
+    public Hub() {
+    }
+}
