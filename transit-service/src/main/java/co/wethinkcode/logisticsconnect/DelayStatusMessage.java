b97b2ee146c37c5fa37416c6b653aab7f8e520c5
mariam mawani <mariammawani@gmail.com>
2026-09-24 20:22:12 +0200

    Add: fields

---
+++ transit-service/src/main/java/co/wethinkcode/logisticsconnect/DelayStatusMessage.java
@@ -0,0 +1,11 @@
+package co.wethinkcode.logisticsconnect;
+
+public class DelayStatusMessage {
+
+    public String hubId;
+    public int stage;
+    public String timestamp;
+
+    public DelayStatusMessage() {
+    }
+}
